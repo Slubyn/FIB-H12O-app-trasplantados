e61a20bae4962aedc8b481019bf45a9f4567b5b2
Slubyn <rebeca28sirbu@gmail.com>
2025-04-02 14:35:44 +0200

    Pantalla dashboard actualizada

---
+++ frontend/app/(tabs)/dashboard.tsx
@@ -0,0 +1,158 @@
+import React, { useState }  from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity,  NativeScrollEvent, NativeSyntheticEvent, Dimensions, Image } from 'react-native';
+import { Ionicons, FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+
+const screenWidth = Dimensions.get('window').width;
+
+const Dashboard: React.FC = () => {
+  //para que los dots se desplacen
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const scrollX = event.nativeEvent.contentOffset.x;
+    const index = Math.round(scrollX / screenWidth * 0.7 + 20); // 150 de ancho + 15 de margen
+    setActiveIndex(index);
+  };
+
+  const cards = [
+    { number: '01', text: 'Inmunosupresión' },
+    { number: '02', text: 'Automedicación' },
+    { number: '03', text: 'Salud sexual' },
+    { number: '04', text: 'Alimentación' },
+  ];
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Hola, "nombre_usuario"</Text>
+
+      <Text style={styles.subtitle}>Guía de recomendaciones</Text>
+
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.carousel}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+      >
+        {cards.map((card, index) => (
+          <View key={index} style={styles.card}>
+            <Text style={styles.cardNumber}>{card.number}</Text>
+            <Text style={styles.cardText}>{card.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
+
+      {/* Puntos dinámicos */}
+      <View style={styles.dotsContainer}>
+        {cards.map((_, index) => (
+          <View
+            key={index}
+            style={index === activeIndex ? styles.dotActive : styles.dot}
+          />
+        ))}
+      </View>
+
+      {/* Utilidades */}
+      <Text style={styles.subtitle}>Utilidades</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Ionicons name="medkit" size={24} color="black" />
+        <Text style={styles.buttonText}>Medicación</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <FontAwesome5 name="calendar-check" size={24} color="black" />
+        <Text style={styles.buttonText}>Próximas citas</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <MaterialIcons name="favorite" size={24} color="black" />
+        <Text style={styles.buttonText}>Tensión arterial / glucosa</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <MaterialIcons name="vaccines" size={24} color="black" />
+        <Text style={styles.buttonText}>Campañas vacunación</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+export default Dashboard;
+
+//css
+const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+      paddingTop: 60,
+      backgroundColor: '#fff',
+      flexGrow: 1,
+    },
+    title: {
+      fontSize: 26,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    subtitle: {
+      marginTop: 30,
+      fontSize: 20,
+      fontWeight: '600',
+    },
+    carousel: {
+      marginTop: 15,
+      flexDirection: 'row',
+    },
+    card: {
+      width: screenWidth * 0.5,
+      height: 130,
+      borderRadius: 12,
+      backgroundColor: '#e0e0e0',
+      marginRight: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 10,
+    },
+    cardNumber: {
+      fontSize: 22,
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    cardText: {
+      fontSize: 16,
+      textAlign: 'center',
+      fontWeight: '500',
+    },
+    dotsContainer: {
+      flexDirection: 'row',
+      marginVertical: 12,
+      justifyContent: 'center',
+    },
+    dot: {
+      width: 10,
+      height: 10,
+      backgroundColor: '#ccc',
+      borderRadius: 5,
+      margin: 5,
+    },
+    dotActive: {
+      width: 10,
+      height: 10,
+      backgroundColor: '#000',
+      borderRadius: 5,
+      margin: 5,
+    },
+    button: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 18,
+      backgroundColor: '#f0f0f0',
+      borderRadius: 12,
+      marginTop: 12,
+    },
+    buttonText: {
+      marginLeft: 15,
+      fontSize: 16,
+      flexShrink: 1,
+      flexWrap: 'wrap',
+    },
+  });
---
+++ frontend/app/(tabs)/medicacion.tsx
@@ -0,0 +1,25 @@
+/*import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Medicacion: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Pantalla de Medicación</Text>
+    </View>
+  );
+};
+
+export default Medicacion;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});*/
