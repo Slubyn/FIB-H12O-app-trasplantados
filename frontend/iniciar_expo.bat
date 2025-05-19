c660c111d0da69685cab1a387eb2e94a7f841753
alex <alejandrottt1021@gmail.com>
2025-05-19 16:38:09 +0200

    merge de gitignore

--- frontend/iniciar_expo.bat
+++ frontend/iniciar_expo.bat
@@ -1,4 +1,5 @@
 @echo off
 
-set EXPO_DEVTOOLS_LISTEN_ADDRESS=192.168.1.133 && set REACT_NATIVE_PACKAGER_HOSTNAME=192.168.1.133 && npx expo start --host=lan
+set EXPO_DEVTOOLS_LISTEN_ADDRESS= 172.20.10.4
+ && set REACT_NATIVE_PACKAGER_HOSTNAME= 172.20.10.4 && npx expo start --host=lan
 
